be25f6aa3d3482bd2b0d4d7cde66bcc967ffffd1
Anton Kuryan <a.kuryan@bylectric.com>
2015-09-21 09:07:22 +0300

    Started

---
+++ linux.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+###
+# ATM - nothing here
+###
---
+++ windows.cmd
@@ -0,0 +1,10 @@
+REM First install chocolatey
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+choco install logparserstudio notepadplusplus dotnet4.5 -y
+
+REM Not fully work here
+choco install steam origin -y
+REM Need uplay also
+REM Some IDEs
+choco install visualstudiocode vs2013.4
